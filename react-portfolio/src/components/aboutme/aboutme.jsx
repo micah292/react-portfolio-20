00241a323e8dbabc41356ce9099ca70eb058c826
Micah Sharifi <sharifimicah17@gmail.com>
2023-05-02 20:15:06 -0700

    setting up

---
+++ react-portfolio/src/components/aboutme/aboutme.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./about.css"
+
+const aboutme = () => {
+  return (
+    <div>aboutme</div>
+  )
+}
+
+export default aboutme
---
+++ react-portfolio/src/components/contactinfo/contactinfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./contactinfo.css"
+const contactinfo = () => {
+  return (
+    <div>contactinfo</div>
+  )
+}
+
+export default contactinfo
